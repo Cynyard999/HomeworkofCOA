23811fd161e9023b18dca1997989140ebe5f20f1
Cynyard999 <CynyardQiu@outlook.com>
2019-10-19 21:12:38 +0800

    Upload HW3

---
+++ HW3/COA19-framework/src/main/java/cpu/alu/ALU.java
@@ -0,0 +1,310 @@
+package cpu.alu;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+import transformer.Transformer;
+
+import java.util.Arrays;
+
+/**
+ * Arithmetic Logic Unit
+ * ALU封装类
+ * TODO: 加减乘除
+ */
+public class ALU {
+
+	/**
+	 * 返回两个二进制整数的乘积(结果直接截取后32位)
+     * 要求使用布斯乘法计算
+	 * @param src 32-bits
+	 * @param dest 32-bits
+	 * @return 32-bits
+	 */
+
+
+	// 模拟寄存器中的进位标志位
+	private String CF = "0";
+
+	// 模拟寄存器中的溢出标志位
+	private String OF = "0";
+
+	public void setOF(String OF) {
+		this.OF = OF;
+	}
+
+	public void setCF(String CF) {
+		this.CF = CF;
+	}
+
+	public String getCF() {
+		return CF;
+	}
+
+	public String getOF() {
+		return OF;
+	}
+
+	//与计算 overload
+	char and(char i,char j){
+		if (i==j&i=='1'){
+			return '1';
+		}
+		return '0';
+	}
+	char and(char i,char j,char k){
+		if (i==j&i=='1'&j==k){
+			return '1';
+		}
+		return '0';
+	}
+	String and(String src, String dest) {
+		char [] result = new char[32];
+		for (int i=0;i<32;i++){
+			result[i] = and(src.charAt(i),dest.charAt(i));
+		}
+		return String.valueOf(result);
+	}
+
+	//或计算 overload
+	char or(char i,char j){
+		if (i==j&i=='0'){
+			return '0';
+		}
+		return '1';
+	}
+	char or(char i,char j,char k){
+		if (i==j&i=='0'&j==k){
+			return '0';
+		}
+		return '1';
+	}
+	String or(String src, String dest) {
+		char [] result = new char[32];
+		for (int i=0;i<32;i++){
+			result[i] = or(src.charAt(i),dest.charAt(i));
+		}
+		return String.valueOf(result);
+	}
+
+	//取反操作
+	String not(String str){
+		char[] temp = new char[str.length()];
+		for (int i=0;i<str.length();i++){
+			temp[i] = str.charAt(i)=='1'?'0':'1';
+		}
+		return String.valueOf(temp);
+	}
+	char not(char a){
+		return a=='1'?'0':'1';
+	}
+	//取相反数操作
+
+	String findComplement(String str){
+		int length = str.length();
+		StringBuilder stringBuilder = new StringBuilder();
+		for (int i=0;i<length-1;i++){
+			stringBuilder.append("0");
+		}
+		stringBuilder.append("1");
+		return add(not(str),stringBuilder.toString());//返回取反+1的值
+	}
+	//add two integer,需要相同长度
+	String add(String src, String dest) {
+		setCF("0");//每个add前设置为0
+		setOF("0");
+		int length = src.length();
+		char [] sum = new char[length];
+		char c ='0';
+		char x=0;
+		char y=0;
+		for (int i=length-1;i>=0;i--){
+			x = src.charAt(i);
+			y = dest.charAt(i);
+			char temp = x==y?'0':'1';
+			sum[i] = temp==c?'0':'1';
+			c = or(and(x,c),and(y,c),and(x,y));
+		}
+        if (c=='1'){
+            setCF("1");
+        }
+        if (x==y&x!=sum[0]){
+            setOF("1");
+        }
+		return String.valueOf(sum);
+	}
+
+	//sub two integer
+	// dest - src
+	String sub(String src, String dest) {
+		if (src.equals(dest)){
+			//return "00000000000000000000000000000000";
+		}
+		return add(dest,findComplement(src));
+	}
+
+	//signed integer mod,dest%src,//余数的符号与被除数相同
+	String imod(String src, String dest) {
+		return new Transformer().intToBinary(String.valueOf(binaryToreal_int(dest)%binaryToreal_int(src)));
+	}
+	int binaryToreal_int(String code) {
+		if (code==null){
+			return 0;
+		}
+		if(code.charAt(0)=='0'){
+			return Integer.parseInt(code,2);
+		}
+		//如果表示的是负数
+		char[] numbers = code.toCharArray();
+		for (int i=0;i<32;i++){
+			numbers[i] = numbers[i]=='0'?'1':'0';//取反
+		}
+		int inversedNumber = Integer.parseInt(String.valueOf(numbers),2)+1;
+		return 0-inversedNumber;
+	}
+
+	String xor(String src, String dest) {
+		char [] result = new char[32];
+		for (int i=0;i<32;i++){
+			result[i]=src.charAt(i)==dest.charAt(i)?'0':'1';
+		}
+		return String.valueOf(result);
+	}
+
+	String shl(String src, String dest) {//逻辑左移,src移动位数
+		char[] result = new char[dest.length()];
+		int length = dest.length();
+		int l = Integer.parseInt(src,2);
+		if (l>=length){//移动长度大于本身长度
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int i=0;i<length;i++){
+				stringBuilder.append("0");
+			}
+			return stringBuilder.toString();
+		}
+		for (int i=length-1;i>length-1-l;i--){
+			result[i]='0';
+		}
+		for (int i =0;i<=length-1-l;i++){
+			result[i] = dest.charAt(i+l);
+		}
+		return String.valueOf(result);
+	}
+
+	String shr(String src, String dest) {//逻辑右移
+		int length = dest.length();
+		char[] result = new char[length];
+		int l = Integer.parseInt(src,2);
+		if (l>=length){//移动长度大于本身长度
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int i=0;i<length;i++){
+				stringBuilder.append("0");
+			}
+			return stringBuilder.toString();
+		}
+		for (int i=0;i<l;i++){
+			result[i]='0';
+		}
+		for (int i =l;i<=length-1;i++){
+			result[i] = dest.charAt(i-l);
+		}
+		return String.valueOf(result);
+	}
+
+	String sal(String src, String dest) {//算术左移
+		return shl(src,dest);
+	}
+
+
+	String sar(String src, String dest) {//算术右移
+		int length = dest.length();
+		char[] result = new char[length];
+		int l = Integer.parseInt(src,2);
+		if (l>length){
+			StringBuilder stringBuilder = new StringBuilder();
+			for (int i=0;i<length;i++){
+				stringBuilder.append(0);
+			}
+			return stringBuilder.toString();
+		}
+		for (int i=0;i<l;i++){
+			result[i]=dest.charAt(0)=='1'?'1':'0';
+		}
+		for (int i =l;i<=length-1;i++){
+			result[i] = dest.charAt(i-l);
+		}
+		return String.valueOf(result);
+	}
+
+	String rol(String src, String dest) {//循环左移
+		char[] result = new char[32];
+		int l = Integer.parseInt(src,2);
+		while (l>=32)
+			l-=32;
+		for (int i=31;i>31-l;i--){
+			result[i]=dest.charAt(i-32+l);
+		}
+		for (int i =0;i<=31-l;i++){
+			result[i] = dest.charAt(i+l);
+		}
+		return String.valueOf(result);
+	}
+
+	String ror(String src, String dest) {//循环右移
+		char[] result = new char[32];
+		int l = Integer.parseInt(src,2);
+		while (l>=32)
+			l-=32;
+		for (int i=0;i<l;i++){
+			result[i]=dest.charAt(31-i);
+		}
+		for (int i =l;i<=31;i++){
+			result[i] = dest.charAt(i-l);
+		}
+		return String.valueOf(result);
+	}
+
+
+
+	String product;
+
+	public void setProduct(String product) {
+		this.product = product;
+	}
+	String Y;
+	public void setY(String y) {
+		Y = y;
+	}
+	public void rightShift_setProductandY(){
+		StringBuilder stringBuilder1;
+		stringBuilder1 = new StringBuilder(product);
+		stringBuilder1.append(Y);
+		setProduct(sar("01",stringBuilder1.toString()).substring(0,32));
+		setY(sar("01",stringBuilder1.toString()).substring(32));
+	}
+
+
+	String mul (String src, String dest){//x*y
+	    int times = src.length();
+		StringBuilder stringBuilder = new StringBuilder(dest);
+		stringBuilder.append(0);
+		setY(stringBuilder.toString());//位长度为32
+		setProduct("00000000000000000000000000000000");
+		for (int i = 0;i<times;i++){
+	    	if (Y.charAt(32)=='0'&Y.charAt(31)=='1'){//-1
+	    		setProduct(sub(src,product));
+				rightShift_setProductandY();
+			}
+	    	else if (Y.charAt(32)==Y.charAt(31)){//0
+	    		rightShift_setProductandY();
+			}
+	    	else {//1
+	    		setProduct(add(src,product));
+	    		rightShift_setProductandY();
+			}
+		}
+		StringBuilder result = new StringBuilder(product);
+		result.append(Y);
+        return result.toString().substring(32,64);//取补充位之前的32位
+    }
+
+
+}
---
+++ HW3/COA19-framework/src/main/java/cpu/alu/FPU.java
@@ -0,0 +1,83 @@
+package cpu.alu;
+
+import util.IEEE754Float;
+
+import java.security.AlgorithmConstraints;
+
+/**
+ * floating point unit
+ * 执行浮点运算的抽象单元
+ * 浮点数精度：使用4位保护位进行计算，计算完毕直接舍去保护位
+ * TODO: 浮点数运算
+ */
+public class FPU {
+
+    /**
+     * compute the float mul of a * b
+     * 分数部分(23 bits)的计算结果直接截取前23位
+     */
+    boolean a_Isregular = true;
+    boolean b_Isregular = true;
+    ALU alu = new ALU();
+    String mul(String a, String b) {
+        StringBuilder stringBuilder = new StringBuilder();
+        if (a.charAt(0)!=b.charAt(0)){//负数
+            stringBuilder.append(1);
+        }
+        else stringBuilder.append(0);
+        if ((!a.substring(1).contains("1"))||(!b.substring(1).contains("1"))){
+            if (a.substring(1,9).equals("11111111")||(b.substring(1,9).equals("11111111"))){
+                return "(0|1){1}1{8}(0+1+|1+0+)(0|1)*";
+            }
+            stringBuilder.append("0000000000000000000000000000000");
+            return stringBuilder.toString();
+        }
+        String fractionX = "1"+a.substring(9)+"0000";
+        String fractionY = "1"+b.substring(9)+"0000";
+        if (a.substring(1,9).equals("00000000")){
+            a_Isregular = false;
+            fractionX = "0"+a.substring(9)+"0000";
+        }
+        if (b.substring(1,9).equals("00000000")){
+            fractionY = "0"+b.substring(9)+"0000";
+            b_Isregular = false;
+        }
+        //先相加，再减去127(化成9位补码的加减）
+        String add_result = alu.add("0"+a.substring(1,9),"0"+b.substring(1,9));
+        String exponent = alu.add("110000001",add_result);
+        //如何判断溢出？
+        if (exponent.charAt(0)=='1'&alu.getCF().equals("1")){//如果减出来是负数
+            stringBuilder.append("00000000");
+            stringBuilder.append("00000000000000000000000");
+            return stringBuilder.toString();
+        }
+        if (exponent.charAt(0)=='1'&alu.getCF().equals("0")){//如果减出来的数大于255
+            stringBuilder.append("11111111");
+            stringBuilder.append("00000000000000000000000");
+            return stringBuilder.toString();
+        }
+
+        stringBuilder.append(exponent.substring(1));
+        String productAndY = "0000000000000000000000000000"+fractionY;//保护位
+        for (int i=0;i<28;i++){
+            if (productAndY.endsWith("0")){
+                productAndY = alu.shr("01",productAndY);//逻辑右移
+            }
+            else {
+                productAndY = alu.add(fractionX+"0000000000000000000000000000",productAndY);
+                productAndY = alu.shr("01",productAndY);
+            }
+        }
+        String fraction;
+        fraction = productAndY.substring(0,23);
+        if (a_Isregular||b_Isregular){//只有一个为regular
+            fraction = productAndY.substring(1,24);
+        }
+        if (a_Isregular&b_Isregular){//两个都为regular
+            fraction = productAndY.substring(2,25);
+        }
+        stringBuilder.append(fraction);
+        return stringBuilder.toString();
+    }
+
+}
